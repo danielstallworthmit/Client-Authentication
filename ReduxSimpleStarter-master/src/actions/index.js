2cfbc91152e3b4c0ebac0f5ef4e64d28ca0190c8
danielstallworthmit <danielstallworthmit@gmail.com>
2017-09-24 23:29:29 -0700

    Add auth reducer to change state when user becomes authenticated

--- ReduxSimpleStarter-master/src/actions/index.js
+++ ReduxSimpleStarter-master/src/actions/index.js
@@ -1,10 +1,23 @@
 import axios from 'axios';
+import { browserHistory } from 'react-router';
 
 const ROOT_URL = 'http://localhost:3090';
+export const AUTH_USER = 'AUTH_USER';
+export const UNAUTH_USER = 'UNAUTH_USER';
 
 export function signinUser({email, password}) {
     return function(dispatch) {
         // Submit email and pw to server
-        axios.post(`${ROOT_URL}/signin`, { email, password });
+        axios.post(`${ROOT_URL}/signin`, { email, password })
+            .then(res => {
+                // If req good then update auth state
+                dispatch({ type: AUTH_USER });
+                // Save jwt token
+                // redirect to the /feature route
+                browserHistory.push('/feature');
+            })
+            .catch(() => {
+                // Req bad -> show error to user
+            })
     }
 }
