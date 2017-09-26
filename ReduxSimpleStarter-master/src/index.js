7fbbb4f2471035e57b0862bde2dfbbc198b0c401
danielstallworthmit <danielstallworthmit@gmail.com>
2017-09-26 00:33:15 -0700

    Add HOC to handle protection of routes when user is not logged in

--- ReduxSimpleStarter-master/src/index.js
+++ ReduxSimpleStarter-master/src/index.js
@@ -6,11 +6,14 @@ import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 import ReduxThunk from 'redux-thunk';
 
 import App from './components/app';
+import RequireAuth from './components/auth/require_auth';
 import reducers from './reducers';
 
+import Welcome from './components/welcome';
 import Signin from './components/auth/signin';
 import Signout from './components/auth/signout';
 import Signup from './components/auth/signup';
+import Feature from './components/auth/feature';
 
 const createStoreWithMiddleware = applyMiddleware(ReduxThunk)(createStore);
 
@@ -18,9 +21,11 @@ ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <Router history={browserHistory}>
       <Route path='/' component={App}>
+        <IndexRoute component={Welcome} />
         <Route path='signin' component={Signin} />
         <Route path='signout' component={Signout} />
         <Route path='signup' component={Signup} />
+        <Route path='feature' component={RequireAuth(Feature)} />
       </Route>
     </Router>
   </Provider>
