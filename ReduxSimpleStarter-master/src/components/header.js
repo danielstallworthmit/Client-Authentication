30b78d647a2951ef4e81f7cf60abbfb35476e0a0
danielstallworthmit <danielstallworthmit@gmail.com>
2017-09-25 22:48:08 -0700

    Update header to show sign out when signed in and sign in and sign up when logged out

--- ReduxSimpleStarter-master/src/components/header.js
+++ ReduxSimpleStarter-master/src/components/header.js
@@ -1,17 +1,42 @@
 import React from 'react';
+import { Link } from 'react-router'
+import { connect } from 'react-redux';
 
 class Header extends React.Component {
+    renderLinks() {
+        // User is logged in, show sign out link 
+        if (this.props.authenticated) {
+            return (
+                <li className="nav-item">
+                    <Link className="nav-link" to="/signout">Sign Out</Link>
+                </li>
+            )
+        } else {
+            // Show link to sign in or sign up when user is signed out
+            return [
+            <li className="nav-item">
+                <Link className="nav-link" to="/signin">Sign In</Link>
+            </li>,
+            <li className="nav-item">
+                <Link className="nav-link" to="/signup">Sign Up</Link>
+            </li>
+            ]
+        }
+    }
     render() {
         return (
             <nav className="navbar navbar-light">
+                <Link to="/" className="navbar-brand">Redux Auth</Link>
                 <ul className="nav navbar-nav">
-                    <li className="nav-item">
-                        Sign In
-                    </li>
+                    {this.renderLinks()}
                 </ul>
             </nav>
         );
     }
 }
 
-export default Header;
+const mapStateToProps = state => {
+    return { authenticated: state.auth.authenticated };
+}
+
+export default connect(mapStateToProps)(Header);
